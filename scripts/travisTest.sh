d4773727a55d170c1226fd089bca0cb0933ad473
Panagiotis Roubatsis <panagiotis.roubatsis@ibm.com>
2019-03-13 09:36:26 -0400

    Fix image and service names

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -19,7 +19,7 @@ helm repo add ibm-charts https://raw.githubusercontent.com/IBM/charts/master/rep
 
 printf "\nhelm install ... name-app\n"
 helm install --name name-app \
-    --set image.repository=registry.ng.bluemix.net/[your-namespace]/name \
+    --set image.repository=name \
     --set image.tag=1.0-SNAPSHOT \
     --set service.port=9080 \
     --set service.targetPort=9080 \
@@ -29,7 +29,7 @@ helm install --name name-app \
 
 printf "\nhelm install ... ping-app\n"
 helm install --name ping-app \
-    --set image.repository=registry.ng.bluemix.net/[your-namespace]/ping \
+    --set image.repository=ping \
     --set image.tag=1.0-SNAPSHOT \
     --set service.port=9080 \
     --set service.targetPort=9080 \
@@ -55,13 +55,15 @@ printf "\ncurl http://$GUIDE_IP:$GUIDE_NAME_PORT/api/name\n"
 curl http://$GUIDE_IP:$GUIDE_NAME_PORT/api/name
 
 printf "\ncurl http://$GUIDE_IP:$GUIDE_PING_PORT/api/ping/name-service\n"
-curl http://$GUIDE_IP:$GUIDE_PING_PORT/api/ping/name-service
+curl http://$GUIDE_IP:$GUIDE_PING_PORT/api/ping/name-app-ibm-open-libert
 
-printf "\nmvn verify -Ddockerfile.skip=true -Dcluster.ip=$GUIDE_IP\n"
-mvn verify -Ddockerfile.skip=true -Dcluster.ip=$GUIDE_IP
+printf "\nmvn verify -Ddockerfile.skip=true -Dcluster.ip=[ip-address] -Dname.node.port=[name-node-port] -Dping.node.port=[ping-node-port] -Dname.kube.service=name-app-ibm-open-libert\n"
+mvn verify -Ddockerfile.skip=true -Dcluster.ip=$GUIDE_IP -Dname.node.port=$GUIDE_NAME_PORT -Dping.node.port=$GUIDE_PING_PORT -Dname.kube.service=name-app-ibm-open-libert
+
 
 printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)\n"
 kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)
 
 printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)\n" 
 kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep ping)
+
