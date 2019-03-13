8ccc4fd3c7e5164e858d9b869f3821f15bcb6199
Panagiotis Roubatsis <panagiotis.roubatsis@ibm.com>
2019-03-13 08:59:02 -0400

    Install helm on travis and use helm to deploy microservices for tests

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -9,8 +9,28 @@
 printf "\nmvn -q package\n"
 mvn -q package
 
-printf "\nkubectl apply -f kubernetes.yaml\n"
-kubectl apply -f kubernetes.yaml
+printf "\nhelm init\n"
+helm init
+
+printf "\nhelm install ... name-app\n"
+helm install --name name-app \
+    --set image.repository=registry.ng.bluemix.net/[your-namespace]/name \
+    --set image.tag=1.0-SNAPSHOT \
+    --set service.port=9080 \
+    --set service.targetPort=9080 \
+    --set ssl.enabled=false \
+    ibm-charts/ibm-open-liberty
+
+
+printf "\nhelm install ... ping-app\n"
+helm install --name ping-app \
+    --set image.repository=registry.ng.bluemix.net/[your-namespace]/ping \
+    --set image.tag=1.0-SNAPSHOT \
+    --set service.port=9080 \
+    --set service.targetPort=9080 \
+    --set ssl.enabled=false \
+    ibm-charts/ibm-open-liberty
+
 
 printf "\nsleep 120\n"
 sleep 120
@@ -18,17 +38,22 @@ sleep 120
 printf "\nkubectl get pods\n"
 kubectl get pods
 
-printf "\nminikube ip\n"
-echo `minikube ip`
+GUIDE_IP=`minikube ip`
+GUIDE_NAME_PORT=`kubectl get service name-app-ibm-open-libert -o jsonpath="{.spec.ports[0].nodePort}"`
+GUIDE_PING_PORT=`kubectl get service ping-app-ibm-open-libert -o jsonpath="{.spec.ports[0].nodePort}"`
 
-printf "\ncurl http://`minikube ip`:31000/api/name\n"
-curl http://`minikube ip`:31000/api/name
+printf "\nMinikube IP: $GUIDE_IP\n"
+printf "\nName Port: $GUIDE_NAME_PORT\n"
+printf "\nPing Port: $GUIDE_PING_PORT\n"
 
-printf "\ncurl http://`minikube ip`:32000/api/ping/name-service\n"
-curl http://`minikube ip`:32000/api/ping/name-service
+printf "\ncurl http://$GUIDE_IP:$GUIDE_NAME_PORT/api/name\n"
+curl http://$GUIDE_IP:$GUIDE_NAME_PORT/api/name
 
-printf "\nmvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip`\n"
-mvn verify -Ddockerfile.skip=true -Dcluster.ip=`minikube ip`
+printf "\ncurl http://$GUIDE_IP:$GUIDE_PING_PORT/api/ping/name-service\n"
+curl http://$GUIDE_IP:$GUIDE_PING_PORT/api/ping/name-service
+
+printf "\nmvn verify -Ddockerfile.skip=true -Dcluster.ip=$GUIDE_IP\n"
+mvn verify -Ddockerfile.skip=true -Dcluster.ip=$GUIDE_IP
 
 printf "\nkubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)\n"
 kubectl logs $(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep name)
