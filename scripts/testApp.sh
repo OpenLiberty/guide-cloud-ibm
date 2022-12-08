07108e6ebf36685168903c0c31012bcf0ba139f9
Gilbert Kwan <gkwan@ca.ibm.com>
2022-12-08 14:53:35 -0500

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -25,7 +25,8 @@ GUIDE_IP=$(minikube ip)
 GUIDE_SYSTEM_PORT=$(kubectl get service system-service -o jsonpath="{.spec.ports[0].nodePort}")
 GUIDE_INVENTORY_PORT=$(kubectl get service inventory-service -o jsonpath="{.spec.ports[0].nodePort}")
 
-curl http://"$GUIDE_IP":"$GUIDE_SYSTEM_PORT"/system/properties
+# if the following curl failed, wait for another 2 minutes
+curl http://"$GUIDE_IP":"$GUIDE_SYSTEM_PORT"/system/properties || sleep 120; kubectl get pods; curl http://"$GUIDE_IP":"$GUIDE_SYSTEM_PORT"/system/properties
 curl http://"$GUIDE_IP":"$GUIDE_INVENTORY_PORT"/inventory/systems/system-service
 
 mvn failsafe:integration-test -Dcluster.ip="$GUIDE_IP" -Dsystem.node.port="$GUIDE_SYSTEM_PORT" -Dinventory.node.port="$GUIDE_INVENTORY_PORT"
