f0927c907812750aa833e4bb8cda5a7cc412d636
Gilbert Kwan <gkwan@ca.ibm.com>
2022-12-08 15:17:04 -0500

    Update startMinikube.sh

--- scripts/startMinikube.sh
+++ scripts/startMinikube.sh
@@ -10,12 +10,12 @@ sysctl fs.protected_regular=0
 # install and set up cri-dockerd
 VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest|grep tag_name | cut -d '"' -f 4|sed 's/v//g')
 echo "$VER"
-wget "https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz"
+wget -q "https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz"
 tar xvf "cri-dockerd-${VER}.amd64.tgz"
 mv cri-dockerd/cri-dockerd /usr/local/bin/
 cri-dockerd --version
-wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service
-wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket
+wget -q https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service
+wget -q https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket
 mv cri-docker.socket cri-docker.service /etc/systemd/system/
 sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
 systemctl daemon-reload
@@ -24,7 +24,7 @@ systemctl enable --now cri-docker.socket
 
 # install crictl
 VERSION="v1.24.2"
-wget "https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz"
+wget -q "https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz"
 tar zxvf "crictl-$VERSION-linux-amd64.tar.gz" -C /usr/local/bin
 rm -f "crictl-$VERSION-linux-amd64.tar.gz"
 
