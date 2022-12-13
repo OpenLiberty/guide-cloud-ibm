4897a11bd5a8540a4e7643260d865cc79c5fb8aa
Gilbert Kwan <gkwan@ca.ibm.com>
2022-12-13 17:37:58 -0500

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
-../scripts/startMinikube.sh
+#../scripts/startMinikube.sh
 
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package
 
-docker pull icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi
+docker -q pull icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi
 
 docker build --no-cache -t system:1.0-SNAPSHOT system/.
 docker build --no-cache -t inventory:1.0-SNAPSHOT inventory/.
@@ -28,15 +28,15 @@ GUIDE_INVENTORY_PORT=$(kubectl get service inventory-service -o jsonpath="{.spec
 curl http://"$GUIDE_IP":"$GUIDE_SYSTEM_PORT"/system/properties
 curl http://"$GUIDE_IP":"$GUIDE_INVENTORY_PORT"/inventory/systems/system-service
 
-mvn failsafe:integration-test -Dcluster.ip="$GUIDE_IP" -Dsystem.node.port="$GUIDE_SYSTEM_PORT" -Dinventory.node.port="$GUIDE_INVENTORY_PORT"
-mvn failsafe:verify
+mvn -ntp failsafe:integration-test -Dcluster.ip="$GUIDE_IP" -Dsystem.node.port="$GUIDE_SYSTEM_PORT" -Dinventory.node.port="$GUIDE_INVENTORY_PORT"
+mvn -ntp failsafe:verify
 
 kubectl logs "$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)"
 kubectl logs "$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep inventory)"
 
 kubectl delete -f kubernetes.yaml
 
-../scripts/stopMinikube.sh
+#../scripts/stopMinikube.sh
 
 # Clear .m2 cache
 rm -rf ~/.m2
