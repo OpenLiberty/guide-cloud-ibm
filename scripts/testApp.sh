6f2048fde2355cbbb98409e0d5ea361213dd3b1e
Gilbert Kwan <gkwan@ca.ibm.com>
2022-12-14 13:21:59 -0800

    Update testApp.sh - try 2 (#145)
    * Update testApp.sh
    
    * Update testApp.sh
    
    * Update test.yml
    
    * Update testApp.sh
    
    * Update testApp.sh
    
    * Update testApp.sh
    
    * Update testApp.sh
    
    * Update testApp.sh
    
    * Update dailyBuild.sh
    
    * Update dailyBuild.sh
    
    * Update testApp.sh
    
    * Update dailyBuild.sh
    
    * Update dailyBuild.sh
    
    * Update dailyBuild.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 set -euxo pipefail
 
-../scripts/startMinikube.sh
+#../scripts/startMinikube.sh
+minikube start
+minikube status
+#kubectl cluster-info
+#kubectl get services --all-namespaces
+#kubectl config view
+eval "$(minikube docker-env)"
 
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package
 
-docker pull icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi
+docker pull -q icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi
 
 docker build --no-cache -t system:1.0-SNAPSHOT system/.
 docker build --no-cache -t inventory:1.0-SNAPSHOT inventory/.
 
+docker images
+
 sed -i 's|us.icr.io/\[your-namespace\]/||g' kubernetes.yaml
+
+cat kubernetes.yaml
 
 kubectl apply -f kubernetes.yaml
 
@@ -28,15 +38,17 @@ GUIDE_INVENTORY_PORT=$(kubectl get service inventory-service -o jsonpath="{.spec
 curl http://"$GUIDE_IP":"$GUIDE_SYSTEM_PORT"/system/properties
 curl http://"$GUIDE_IP":"$GUIDE_INVENTORY_PORT"/inventory/systems/system-service
 
-mvn failsafe:integration-test -Dcluster.ip="$GUIDE_IP" -Dsystem.node.port="$GUIDE_SYSTEM_PORT" -Dinventory.node.port="$GUIDE_INVENTORY_PORT"
-mvn failsafe:verify
+mvn -ntp failsafe:integration-test -Dcluster.ip="$GUIDE_IP" -Dsystem.node.port="$GUIDE_SYSTEM_PORT" -Dinventory.node.port="$GUIDE_INVENTORY_PORT"
+mvn -ntp failsafe:verify
 
 kubectl logs "$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)"
 kubectl logs "$(kubectl get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep inventory)"
 
 kubectl delete -f kubernetes.yaml
 
-../scripts/stopMinikube.sh
+#../scripts/stopMinikube.sh
+eval "$(minikube docker-env -u)"
+minikube stop
 
 # Clear .m2 cache
 rm -rf ~/.m2
